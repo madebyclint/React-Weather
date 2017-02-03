23fb1fdca072dbccb02da95bd0770269c71bd261
Clint Bush <clint.bush@homesite.com>
2017-02-03 15:26:01 -0800

    Through Section 5 Lecture 35: Wired Data

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,23 @@
+var axios = require('axios');
+
+// API Key: d964c536b7e4ab4ac56450c348047d2b
+// API Url: http://api.openweathermap.org/data/2.5/weather?appid=d964c536b7e4ab4ac56450c348047d2b&units=imperial
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=d964c536b7e4ab4ac56450c348047d2b&units=imperial';
+
+module.exports = {
+    getTemp: function (location) {
+        var encodedLocation = encodeURIComponent(location);
+        var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+        return axios.get(requestUrl).then(function (res) {
+            if (res.data.cod && res.data.message) {
+                throw new Error(res.data.message);
+            } else {
+                return res.data.main.temp;
+            }
+        }, function (res) {
+            throw new Error(res.data.message);
+        });
+    }
+}
