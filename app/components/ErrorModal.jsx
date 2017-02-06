1410cb3afb09ed8922f4618791cfea72980e584e
Clint Bush <clint@madebyclint.com>
2017-02-05 22:01:47 -0800

    Add ErrorModal component.

---
+++ app/components/ErrorModal.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+
+var ErrorModal = React.createClass({
+    componentDidMount: function () {
+        var modal = new Foundation.Reveal($('#error-modal'));
+        modal.open();
+    },
+    render: function () {
+        return (
+            <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+                <h4>Some title</h4>
+                <p>Our error message</p>
+                <p>
+                    <button className="button hollow" data-close="">
+                        Okay
+                    </button>
+                </p>
+            </div>
+        );
+    }
+});
+
+module.exports = ErrorModal;
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,30 +1,36 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var ErrorModal = require('ErrorModal');
 var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
     getInitialState: function () {
         return {
-            isLoading: false
+            isLoading: false,
         }
     },
     handleSearch: function (location) {
         var that = this;
-        this.setState({isLoading: true});
+        this.setState({
+            isLoading: true,
+            errorMessage: undefined
+        });
         openWeatherMap.getTemp(location).then(function (temp) {
             that.setState({
                 location: location,
                 temp: temp,
                 isLoading: false
             });
-        }, function (errorMessage) {
-            alert(errorMessage);
-            that.setState({isLoading: false});
+        }, function (e) {
+            that.setState({
+                isLoading: false,
+                errorMessage: e.message
+            });
         });
     },
     render: function () {
-        var {isLoading, temp, location} = this.state;
+        var {isLoading, temp, location, errorMessage} = this.state;
 
         function renderMessage () {
             if (isLoading) {
@@ -33,12 +39,22 @@ var Weather = React.createClass({
                 return <WeatherMessage temp={temp} location={location}/>;
             }
         }
+
+        function renderError () {
+            if (typeof errorMessage === 'string') {
+                return (
+                    <ErrorModal/>
+                );
+            }
+        }
+
         return (
             // Only one parent container can go here - so one div
             <div>
                 <h1 className="text-center">Get Weather</h1>
                 <WeatherForm onSearch={this.handleSearch}/>
                 {renderMessage()}
+                {renderError()}
             </div>
         );
     }
