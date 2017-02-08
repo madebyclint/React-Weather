69e9e754d804b76c36034ec8fcf112d8e7a2c00a
Clint Bush <clint.bush@homesite.com>
2017-02-07 16:47:04 -0800

    Work on errorModal

--- app/app.jsx
+++ app/app.jsx
@@ -7,7 +7,7 @@ var About = require('About');
 var Examples = require('Examples');
 
 // Load Foundation
-require('style!css!foundation-sites/dist/foundation.min.css');
+require('style!css!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
 
 ReactDOM.render(
